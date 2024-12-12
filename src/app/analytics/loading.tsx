564139375fc994e638a82ef6cc7f57086002a688
Jayden Richardson <jsrich@hotmail.co.nz>
2024-12-12 13:19:30 +0000

    rebuild

--- src/app/analytics/loading.tsx
+++ src/app/analytics/loading.tsx
@@ -1,6 +1,7 @@
 import { Container } from "~/components/ui/Container";
 import { Spinner } from "~/components/ui/Spinner";
 
+
 export default function AnalyticsLoading() {
   return (
     <Container>
